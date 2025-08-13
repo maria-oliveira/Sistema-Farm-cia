3a47cc27cedbb4454830ea46475d453d56725f5b
Maria Eduarda T M Oliveira <maria.tmiller2006@gmail.com>
2025-08-13 12:30:44 -0300

    Sistema farmacia

--- src/main/java/com/farmacia/util/DatabaseConnection.java
+++ src/main/java/com/farmacia/util/DatabaseConnection.java
@@ -5,13 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/farmacia?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
-    private static final String USER = "farmacia_user";
-    private static final String PASSWORD = "minhasenha123";
-    ;
+    private static final String URL = "jdbc:mysql://localhost:3306/farmacia"
+            + "?allowPublicKeyRetrieval=true"
+            + "&useSSL=false"
+            + "&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "201607@";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver JDBC do MySQL não encontrado!", e);
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
